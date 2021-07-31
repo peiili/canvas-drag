6beec6cd3e53560c53a3a0e22e4755f1263beb33
peiili <1017935195@qq.com>
2021-07-31 19:39:28 +0800

    03Day

---
+++ 03Day/main.js
@@ -0,0 +1,131 @@
+const canvas = document.getElementById("canvas");
+const canvasWidth = 500;
+const canvasHeight = 500;
+let i = 0;
+
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+
+const context = canvas.getContext("2d");
+const canvasStory = [];
+
+
+initCanvas()
+
+function initCanvas() {
+  context.clearRect(0, 0, canvasWidth, canvasHeight);
+  context.fillStyle = "yellow";
+  context.fillRect(0, 0, canvasWidth, canvasHeight);
+}
+
+// 创建矩形
+
+class drawRect {
+  constructor(x, y, width, height, color, hover) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.hover = hover;
+  }
+  draw() {
+    context.fillStyle = this.color;
+    // console.log(this.hover)
+    if (this.hover) {
+      // console.log('this.hover')
+      context.shadowColor = 'black';
+      context.shadowBlur = '10';
+    } else {
+      context.shadowColor = '';
+      context.shadowBlur = '0';
+    }
+    context.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+function addRect() {
+  const width = 100;
+  const height = 60;
+  const color = 'red';
+  const x = Math.random() * (canvasWidth - width);
+  const y = Math.random() * (canvasHeight - height);
+  const imgData = {
+    index:canvasStory.length,
+    x,
+    y,
+    width,
+    height,
+    color,
+    hover: false
+  };
+  renderCanvas(imgData)
+  canvasStory.push(imgData)
+}
+
+
+function deleteFirst() {
+  canvasStory.shift();
+  initCanvas()
+  for (let i = 0; i < canvasStory.length; i++) {
+    const element = canvasStory[i];
+    renderCanvas(element);
+  }
+}
+// canvas.addEventListener("click", mouseDown, false);
+function renderCanvas(data) {
+  const rect = new drawRect(data.x, data.y, data.width, data.height, data.color, data.hover);
+  rect.draw();
+}
+// 鼠标悬浮
+function mouseMove(event) {
+  const mouseX = event.pageX
+  const mouseY = event.pageY
+  let hasHover = -1
+  initCanvas()
+  // 记录下hover 的对象并初始化所有内容
+  canvasStory.forEach((e,index) => {
+    const targetminX = e.x
+    const targetmaxX = e.x + e.width
+    const targetminY = e.y
+    const targetmaxY = e.y + e.height
+    const atRangeX = mouseX > targetminX && mouseX < targetmaxX
+    const atRangeY = mouseY > targetminY && mouseY < targetmaxY
+    e.hover = false
+    if (atRangeY && atRangeX) {
+      // 记录下最后一个hover
+      hasHover = index
+    }
+  })
+  if(hasHover>-1){
+    canvasStory[hasHover].hover = true
+  }
+ // 将处理后的数据再次渲染到画布
+  canvasStory.forEach((e) => {
+    renderCanvas(e)
+  })
+}
+// 鼠标点击
+function mouseEnter(event){
+  const mouseX = event.pageX
+  const mouseY = event.pageY
+  canvasStory.forEach(e=>{
+    const targetminX = e.x
+    const targetmaxX = e.x+e.width
+    const targetminY = e.y
+    const targetmaxY = e.y+e.height
+    const atRangeX = mouseX>targetminX&&mouseX<targetmaxX
+    const atRangeY = mouseY>targetminY&&mouseY<targetmaxY
+    if(atRangeY&&atRangeX){
+      console.log(true)
+    }
+  })
+}
+
+const addBtn = document.getElementById("addRect");
+addBtn.addEventListener("click", addRect, false);
+
+const delBtn = document.getElementById("delRect");
+delBtn.addEventListener("click", deleteFirst, false);
+
+canvas.addEventListener('mousemove', mouseMove, false)
